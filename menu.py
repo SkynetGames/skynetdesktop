c688cf79b2bffa160efad02a3e8b686124bc2f63
j4463085 <j4463085@nwytg.net>
2018-12-15 18:13:38 -0500

    Final commit in python

--- menu.py
+++ menu.py
@@ -5,7 +5,8 @@ from download import download
 # Imported Dependencies
 from pprint import pprint
 import tkinter as tk
-from tkinter import Button
+from tkinter import *
+from PIL import ImageTk, Image
 
 
 # Imports info from GUI
@@ -34,9 +35,10 @@ def games(object):
     canvas = getCanvas("bottom")
     t = ""
     for i in range(len(utils.gameData[0])):
-        t = t + utils.gameData[0][i]["alt"] + "\n"
-    label = canvas.create_text(500, 500, text=t, tag="bottom")
-    label.pack
+        img = ImageTk.PhotoImage(Image.open("src/images/" + utils.gameData[0][i]["name"] + ".jpg"))
+        panel = tk.Label(canvas, image = img)
+        #The Pack geometry manager packs widgets in rows or columns.
+        panel.pack(fill="none")
 
 # Creates download buttons for software page
 def software(object):
