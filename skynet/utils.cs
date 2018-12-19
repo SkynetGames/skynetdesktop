fe369bc7b29476ff0f0353b0d11ffcbcd9f37dbd
j4463085 <j4463085@nwytg.net>
2018-12-18 20:04:07 -0500

    basically just changed the background

---
+++ skynet/utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace skynet
+{
+    class Utils
+    {
+        public static string[] Colors = new string[]
+        {
+            "#b0bec5", // Light Blue
+            "#546e7a", // Middle Blue
+            "#263238"  // Dark Blue
+        };
+    }
+}
