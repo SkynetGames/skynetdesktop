0d47120ce71d31e36e4bc1fca5cc9084f9a6e7ac
j4463085 <j4463085@nwytg.net>
2018-12-19 20:44:25 -0500

    Started working on the backend

--- skynet/init.cs
+++ skynet/init.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace skynet
 {
@@ -8,7 +12,52 @@ namespace skynet
     {
         public static void InitMain()
         {
-            Console.WriteLine("Init");
+            // Creates the source folder if it doesn't exists
+            Directory.CreateDirectory("src");
+            string[,] dataFiles = Utils.dataFiles;
+
+            // Checks each of the important files
+            for(int i = 0; i < (dataFiles.GetLength(0) - 1); i++)
+            {
+                if (!File.Exists(dataFiles[i, 0]) || IsBelowThreshold(dataFiles[i,0], 24))
+                {
+                    using (var client = new WebClient())
+                    {
+                        Console.WriteLine("Downloading data from " + dataFiles[i, 1]);
+                        client.DownloadFile(dataFiles[i, 1], dataFiles[i, 0]);
+                    }
+                }
+            }
+            if (!File.Exists(dataFiles[2,0]))
+            {
+                using (var client = new WebClient())
+                {
+                    Console.WriteLine("Downloading data from " + dataFiles[2,1]);
+                    client.DownloadFile(dataFiles[2,0], dataFiles[2,0]);
+                }
+            }
+            ReadJSON();
+
+            // Annoucing that initialization is complete
+            Console.WriteLine("Initialization is complete, starting Skynet");
+        }
+
+        // Generates usable data from JSON files
+        private static void ReadJSON()
+        {
+            Console.WriteLine("Reading game data");
+            Utils.GameData = JArray.Parse(File.ReadAllText(@"src/gameData.json"));
+            Console.WriteLine("Reading software data");
+            Utils.SoftwareData = JArray.Parse(File.ReadAllText(@"src/softwareData.json"));
+            Console.WriteLine("Reading user data");
+            Utils.UserData = JArray.Parse(File.ReadAllText(@"src/gameData.json"));
+        }
+
+        // Checks file age, returns true if file is older than hours
+        private static bool IsBelowThreshold(string filename, int hours)
+        {
+            var threshold = DateTime.Now.AddHours(-hours);
+            return !(File.GetCreationTime(filename) <= threshold);
         }
     }
 }
--- skynet/utils.cs
+++ skynet/utils.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using Newtonsoft.Json.Linq;
 
 namespace skynet
 {
     class Utils
     {
+        // Local and remote locations of all important JSON files
+        public static string[,] dataFiles = new string[,]
+        {
+            { "src/gameData.json", "https://s3.amazonaws.com/skynet-metadata/game_data.json" },
+            { "src/softwareData.json", "https://s3.amazonaws.com/skynet-metadata/software.json" },
+            { "src/userData.json", "https://s3.amazonaws.com/skynet-desktop/usr_prefs.json" }
+        };
+
+        // Lists generated from the JSON files
+        public static JArray GameData;
+        public static JArray SoftwareData;
+        public static JArray UserData;
+
+        // The color pallet of Skynet, this is never used...
         public static string[] Colors = new string[]
         {
             "#b0bec5", // Light Blue
